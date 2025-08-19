080ebee7640a6f2b7399c45309abfe3c004ce4e1
JOALLYFER LOUREIRO CAMARGO <joallyfer.camargo@cs.up.edu.br>
2025-08-18 21:32:39 -0300

    Atualização da API

--- Ecommerce/API/Program.cs
+++ Ecommerce/API/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+//Para criar Funcionalidade - Requisições
+// Precisamos de uma URL/Endereço/Caminho
+// Um método HTTP
+
+app.MapGet("/", () => "Minha Segunda API em C#!");
+
+app.MapGet("/endereco", () => "Outra Funcionalidade");
+
+app.MapPost("/endereco", () => "Funcionalidade do Post");
+
+
 
 app.Run();
